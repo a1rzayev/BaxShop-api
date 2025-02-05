47f17c5b6bd6c729e665f7b8a626d3ed3aa5b092
a1rzayev <asifrzayev2006@example.com>
2025-02-05 21:23:18 +0400

    Add bcrypt(user.password)

--- AuthAPI/src/models/user.js
+++ AuthAPI/src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const Roles = require('../../utils/roles'); 
+const bcrypt = require('bcryptjs');
+const Roles = require('../utils/roles'); 
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
@@ -7,6 +8,16 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   role: { type: String, enum: Object.values(Roles), default: Roles.USER } 
 });
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.matchPassword = function (enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 
