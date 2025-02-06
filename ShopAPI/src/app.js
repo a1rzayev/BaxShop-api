d08ab6a660a38f9bcb83b43c2a818a1569d9542a
a1rzayev <asifrzayev2006@example.com>
2025-02-06 21:01:42 +0400

    Add to app.js(error-handler)

--- ShopAPI/src/app.js
+++ ShopAPI/src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const errorHandler = require("./middleware/error_middleware")
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(cors());
+app.use(errorHandler);
 app.use(express.json());
 
 app.listen(port, () => {
